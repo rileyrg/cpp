6e4f92c58353bcea695f498605dfc6cfe0c9b574
rileyrg <rileyrg@gmx.de>
2021-08-24 22:35:29 +0200

    added 4-2-conrete-types

---
+++ TourOfCppV2/4-2-concrete-types/arithmetic.cpp
@@ -0,0 +1,34 @@
+#include <cstdlib>
+#include <iostream>
+#include <string>
+
+class complex
+{
+private:
+     double re,im;
+public:
+     complex(double r, double i): re{r},im{i}{}
+     complex(double r): re{r},im{0}{}
+     complex(): re{0},im{0}{}
+
+     double real() const {return re;}
+     void real(double d) {re=d;}
+     double img() const {return im;}
+     void img(double i) {im=i;}
+
+     complex & operator+=(complex z){
+          re+=z.re;
+          im+=z.im;
+          return *this;
+     }
+     complex & operator-=(complex z){
+          re-=z.re;
+          im-=z.im;
+          return *this;
+     }};
+
+int main(){
+     complex z={1,0};
+     const complex cz{1,3};
+     z+=cz;
+}
