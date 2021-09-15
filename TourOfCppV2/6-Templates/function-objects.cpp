e473ae27f520179ebac9985d9727b83cf2369206
rileyrg <rileyrg@gmx.de>
2021-09-15 22:24:56 +0200

    added different function object uses

--- TourOfCppV2/6-Templates/function-objects.cpp
+++ TourOfCppV2/6-Templates/function-objects.cpp
@@ -10,7 +10,11 @@ public:
 };
 
 int main(void) {
+     LessThan<int> c{1000} ;
+     auto p = new LessThan<int>{1000} ;
      std::cout <<  (LessThan<int>{2}(1)?"True":"False") << std::endl  ;
      std::cout <<  (LessThan<int>{10}(11)?"True":"False") << std::endl  ;
+     std::cout <<  (c(999)?"True":"False") << std::endl  ;
+     std::cout <<  ((*p)(999)?"True":"False") << std::endl  ;
      return EXIT_SUCCESS;
 }
