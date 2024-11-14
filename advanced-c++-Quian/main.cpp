a4e3bf23334e35c40939b4d8e07ce6d0379e8208
rileyrg <rileyrg@gmx.de>
2024-11-14 22:45:56 +0100

    git test in remote location

--- advanced-c++-Quian/main.cpp
+++ advanced-c++-Quian/main.cpp
@@ -8,7 +8,7 @@ int main() {
   const int *p1 = &i; // data is const , pointer is not
   p1++;
 
-  int *const p2 = nullptr; // pointer is const, data is not.
+  int *const p2 = nullptr; // pointer is const, data is not. git test.
 
   const int *const p3 = nullptr; // data and pointer both const
 }
