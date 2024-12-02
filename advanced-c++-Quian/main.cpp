b8ce7934757bbb5160f176c7a1565c8d76e52f44
rileyrg <rileyrg@gmx.de>
2024-12-02 10:33:44 +0100

    clang-format

--- advanced-c++-Quian/main.cpp
+++ advanced-c++-Quian/main.cpp
@@ -2,29 +2,37 @@
 #include <iostream>
 
 class Dog {
-  int age;
-  std::string name;
+    int age;
+    std::string name;
 
-public:
-  Dog() {
-    age = 3;
-    name = "dummy";
-  }
-  // void setAge(const int a) { age = a; }
-  void setAge(int a) { age = a; }
+    public:
+    Dog() {
+        age = 3;
+        name = "dummy";
+    }
+    // void setAge(const int a) { age = a; }
+    void setAge(int a) {
+        age = a;
+    }
+    // const return value
+    const std::string &getName() const {
+        return name;
+    }
 
-  // const return value
-  const std::string &getName() { return name; }
+    // const functions - no member variables changed
+    void printDogName() const {
+        std::cout << name << "const" << std::endl;
+    }
 
-  // const functions - no member variables changed
-  void printDogName() const { std::cout << name << "const" << std::endl; }
-  // non const version is a different version
-  void printDogName() { std::cout << getName() << "non const" << std::endl; }
+    // non const version is a different version
+    void printDogName() {
+        std::cout << getName() << "non const" << std::endl;
+    }
 };
 
 int main() {
-  Dog d;
-  d.printDogName();
-  const Dog d2;
-  d2.printDogName();
+    Dog d;
+    d.printDogName();
+    const Dog d2;
+    d2.printDogName();
 }
